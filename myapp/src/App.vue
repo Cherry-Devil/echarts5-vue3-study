5a7e982d9b30ecca8bca67e24e4b63f44be296ff
Cherry-Devil <17274823751@163.com>
2023-03-29 09:51:01 +0800

    changes background

--- myapp/src/App.vue
+++ myapp/src/App.vue
@@ -7,7 +7,7 @@ import {provide} from "vue"
 import * as echarts from "echarts"
 import axios from "axios"
 
-// 设置基准路径
+// 设置基准路径（本地）
 // axios.defaults.baseURL="http://118.178.233.180:8888/"
 axios.defaults.baseURL="http://localhost:8888/"
 export default {
@@ -21,7 +21,8 @@ export default {
 
 <style lang="less">
 body{
-  background: url("~@/assets/bg.jpg") top center no-repeat;
+  background: url("~@/assets/blue.jpg") top center no-repeat;
+  
 }
 *{
   margin:0px;
